bd3d0ed2d0cc37fd7ccd518ec5206a8c04eee952
noobcoder123456789 <noobcoder1234567@gmail.com>
2025-12-26 08:47:36 +0700

    Enhance SearchScreenStyles: update alignment of items for improved layout consistency.

--- Front-end/styles/SearchScreenStyles.ts
+++ Front-end/styles/SearchScreenStyles.ts
@@ -64,7 +64,7 @@ const styles = StyleSheet.create({
         display: "flex",
         flexDirection: "row",
         justifyContent: "space-between",
-        alignItems: "flex-end",
+        alignItems: "center",
         marginBottom: 10,
     },
     showMoreText: {
